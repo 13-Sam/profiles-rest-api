b8cd3ab530777b0897763f13c1f286307f3aee5d
sayed <sayedalmamur@gmail.com>
2023-10-21 09:21:54 +0600

    hello api view created

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class HelloAPIView(APIView):
+    '''Test API View'''
+    def get(self, request, format=None):
+        '''Returns a list of APIView features'''
+        an_apiview = [
+            'api is good',
+            'api is sufficient',
+            'api is better',
+            'api is best',
+        ]
+        return Response({'message':'Hello!', 'an_apiview': an_apiview})
